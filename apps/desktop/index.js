0ec77ed20c1a6e40169c2bd196a7c7edca8247f8
Benjamin Wright <benjaminwright@Benjamins-MacBook-Pro.local>
2022-12-08 22:22:25 -0600

    Added vite web app and desktop electron app

---
+++ apps/desktop/index.js
@@ -0,0 +1,71 @@
+const { app, protocol, BrowserWindow } = require('electron');
+const path = require('path');
+const url = require('url')
+const isDev = process.env.IS_DEV === 'true'
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) {
+  app.quit();
+}
+
+const createWindow = () => {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+
+  // and load the index.html of the app.
+  if (!isDev) {
+    mainWindow.loadFile(path.join(__dirname, '../dist/index.html'));
+  } else {
+    mainWindow.loadURL("http://localhost:5173/")
+    mainWindow.webContents.openDevTools();
+  }
+  
+  // mainWindow.loadURL('app://../dist/index.html');
+  // mainWindow.loadURL(url.format({
+  //   pathname: 'index.html',    /* Attention here: origin is path.join(__dirname, 'index.html') */
+  //   protocol: 'file',
+  //   slashes: true
+  // }))
+
+  // Open the DevTools.
+  
+};
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', () => {
+  // protocol.interceptFileProtocol('file', (request, callback) => {
+  //   const url = request.url.substr(7)    /* all urls start with 'file://' */
+  //   callback({ path: path.normalize(`${__dirname}/../dist/${url}`)})
+  // }, (err) => {
+  //   if (err) console.error('Failed to register protocol')
+  // })
+  createWindow()
+});
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and import them here.
