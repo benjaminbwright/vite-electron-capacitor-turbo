224b2ef84a298798e4225ac4aad40c2baf7b763b
Benjamin Wright <benjaminwright@Benjamins-MacBook-Pro.local>
2022-12-16 10:35:20 -0600

    configured electron to copy shared paths to build

--- apps/desktop/forge.config.js
+++ apps/desktop/forge.config.js
@@ -1,8 +1,9 @@
+const { default: rebuild } = require("electron-rebuild")
+const fs = require("fs-extra")
+const path = require("path")
+
 module.exports = {
   packagerConfig: {},
-  rebuildConfig: {
-    buildPath: "../node_modules"
-  },
   makers: [
     {
       name: '@electron-forge/maker-squirrel',
@@ -21,4 +22,26 @@ module.exports = {
       config: {},
     },
   ],
+  hooks: {
+    packageAfterCopy: async (conf, buildPath, electronVersion, platform, arch) => {
+      // copy shared packages to production node_modules
+      const files = fs.readdirSync(path.resolve(buildPath, "node_modules"), {encoding: 'utf8', withFileTypes: true});
+      await files.forEach((module) => {
+        if (module.isSymbolicLink()) {
+          const moduleInBuild = path.resolve(buildPath, "node_modules", module.name);
+          const moduleInDev = path.resolve(path.resolve(__dirname, "node_modules", module.name));
+          const realpath = fs.realpathSync(moduleInDev);
+          fs.rmSync(moduleInBuild, { recursive: true, force: true });
+          fs.copySync(realpath, moduleInBuild, { overwrite: true, recursive: true });
+        }
+      });
+      // rebuild native modules to local node version
+      await rebuild({
+        buildPath,
+        arch,
+        electronVersion,
+        force: true
+      });
+    },
+  }
 };
